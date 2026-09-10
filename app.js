ddae582a865cb6b4f738b45f1794eb609ce60c4d
pixelinkztechnologies-rgb <pixelinkztechnologies@gmail.com>
2026-09-10 15:24:25 +0530

    episode 3 let write code

---
+++ app.js
@@ -0,0 +1,19 @@
+var name ="vasihnavi";
+var a=10;
+var b=20;
+console.log(name);
+console.log(a+b);
+console.log(global);
+console.log(this);
+console.log(globalThis);
+console.log(globalThis === global);
+console.log(globalThis === this);
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(exports);
+// console.log(globalThis === window);
+// console.log(self);
+// console.log(frames)
+
+// console.log(window);

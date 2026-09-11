c735d359bd681319add8e1768d2c4049356f4dcc
pixelinkztechnologies-rgb <pixelinkztechnologies@gmail.com>
2026-09-11 14:15:27 +0530

    episodde 4

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-var name ="vasihnavi";
-var a=10;
-var b=20;
-console.log(name);
-console.log(a+b);
-console.log(global);
-console.log(this);
-console.log(globalThis);
-console.log(globalThis === global);
-console.log(globalThis === this);
-console.log(__dirname);
-console.log(__filename);
-console.log(module);
-console.log(exports);
+// var a=10;
+// var b=20;
+// console.log(name);
+// console.log(a+b);
+// console.log(global);
+// console.log(this);
+// console.log(globalThis);
+// console.log(globalThis === global);
+// console.log(globalThis === this);
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(module);
+// console.log(exports);
 // console.log(globalThis === window);
 // console.log(self);
 // console.log(frames)
-
 // console.log(window);
+
+require('./xyz');
+const {multiply, calculateSum, x} = require('./calculate');
+// const {x,calculateSum} = require('./calculate/sum');
+var name ="vasihnavi";
+console.log(calculateSum(10, 20));
+console.log(multiply(10, 20));
+console.log(x);
